be0a277a752baf3de4fc027c2f4ce3e7178ed4a8
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2023-04-12 00:58:59 +0700

    456yh

--- script.js
+++ script.js
@@ -29,11 +29,12 @@ for (let i = -1; i <= 1; i++) {
 
       let name = "";
       name += (j > 0) ? "U" : (j < 0) ? "D" : "";
-      name += (i > 0) ? "F" : (i < 0) ? "B" : "";
-      name += (k > 0) ? "R" : (k < 0) ? "L" : "";
+      name += (k > 0) ? "F" : (k < 0) ? "B" : "";
+      name += (i > 0) ? "R" : (i < 0) ? "L" : "";
+      name = (name == "") ? "core" : name;
 
       RubikCube[i + 1][j + 1][k + 1] = new Cubie({x: i, y: j, z: k}, side, type, name);
-      // console.log("x: " + i + ", y: " + j + ", z: " + k + ", type: " + type + ", name: " + name);
+      console.log("x: " + i + ", y: " + j + ", z: " + k + ", type: " + type + ", name: " + name);
     }
   }
 }
