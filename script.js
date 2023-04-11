ec065e5ce3069142430769bd8313c222b64455e2
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2023-04-12 00:55:44 +0700

    rhgerh

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 class Cubie {
-  constructor(positions, side) {
+  constructor(positions, side, type, name) {
     this.positions = positions;
     this.side = side;
+    this.type = type;
+    this.name = name;
   }
 }
 
+let side = 10;
 let RubikCube = [];
+
 for (let i = -1; i <= 1; i++) {
   RubikCube[i + 1] = [];
   for (let j = -1; j <= 1; j++) {
     RubikCube[i + 1][j + 1] = [];
     for (let k = -1; k <= 1; k++) {
-      RubikCube[i + 1][j + 1][k + 1] = new Cubie({x: i, y: j, z: k}, 10);
-      console.log("x: " + i + ", y: " + j + ", z: " + k)
+      let type = "";
+      let counts = {};
+
+      [i, j, k].forEach(x => counts[x] = (counts[x] || 0) + 1 );
+
+      switch ( counts["0"] ) {
+        case 3: type = "core"; break;
+        case 2: type = "center"; break;
+        case 1: type = "edge"; break;
+        default: type = "corner";
+      }
+
+      let name = "";
+      name += (j > 0) ? "U" : (j < 0) ? "D" : "";
+      name += (i > 0) ? "F" : (i < 0) ? "B" : "";
+      name += (k > 0) ? "R" : (k < 0) ? "L" : "";
+
+      RubikCube[i + 1][j + 1][k + 1] = new Cubie({x: i, y: j, z: k}, side, type, name);
+      // console.log("x: " + i + ", y: " + j + ", z: " + k + ", type: " + type + ", name: " + name);
     }
   }
 }
